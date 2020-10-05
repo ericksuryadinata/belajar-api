90c6dc9e487f98999120b672cf5e5580d933660f
Erick Surya Dinata <ericksuryadinata@gmail.com>
2020-10-05 08:50:15 +0700

    revert to bad220504

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ControllerRegister;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::name('api.v1.')->prefix('v1')->group(function () {
-    Route::post('/register',[ControllerRegister::class, 'store']);
 });
